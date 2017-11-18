2d17ac8651f7cd63f3375448a3c522dda436382d
MehmedOmerovic <33461525+MehmedOmerovic@users.noreply.github.com>
2017-11-18 14:06:05 +0100

    Add files via upload

---
+++ Zdravo.java
@@ -0,0 +1,16 @@
+
+public class Zdravo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	double povrsina;
+	double poluprecnik;
+	poluprecnik = 20.7;
+			povrsina= poluprecnik * poluprecnik * 3.14;
+			
+		System.out.println("povrsina kruga poluprecnika " + "iznosi " + povrsina);
+		
+	}
+
+}
